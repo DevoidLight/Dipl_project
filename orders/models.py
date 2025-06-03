98412bbb7f933e3b5f3f1aa2a1fc9ab4798ea859
black_nuar <hasaevsuliman6@gmail.com>
2025-06-03 17:35:50 +0300

    First commit

--- orders/models.py
+++ orders/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from products.models import Ribbons, Paints, Tempaltes
 
 # Create your models here.
+class Orders(models.Model):
+    ribbon = models.ForeignKey(Ribbons, on_delete=models.CASCADE, related_name='orders')
+    paint = models.ForeignKey(Paints, on_delete=models.CASCADE, related_name='orders')
+    template = models.ForeignKey(Tempaltes, on_delete=models.CASCADE, related_name='orders')
+    class_list = models.FileField()
+    teacher_name = models.CharField(max_length=128)
+    deploy = models.DateField()
+    school = models.CharField(max_length=128)
+    class_number = models.CharField(max_length=24)
