8cd7e0276c2ae05a39c4a4b3cffb74a1b5eb3502
black_nuar <hasaevsuliman6@gmail.com>
2025-06-03 15:37:06 +0300

    Initial commit

---
+++ products/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+
+# Create your models here.
+class Ribbons(models.Model):
+    color = models.CharField(max_length=48)
+    is_active = models.BooleanField(default=True)
+    length = models.IntegerField()
+    base_price = models.DecimalField(max_digits=10, decimal_places=2)
+
+
+class Paints(models.Model):
+    color = models.CharField(max_length=48)
+    is_active = models.BooleanField(default=True)
+    length = models.IntegerField()
+    base_price = models.DecimalField(max_digits=10, decimal_places=2)
+
+
+class Tempaltes(models.Model):
+    image = models.ImageField(upload_to='products/templates')
